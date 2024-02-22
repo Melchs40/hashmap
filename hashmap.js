74283e2319ff7533a22c386c441d40fa05eb64bc
Melchs40 <samscodingjourney@gmail.com>
2024-02-21 22:46:21 -0600

    Edit hashmap class to add in get, has, remove functions

--- hashmap.js
+++ hashmap.js
@@ -36,4 +36,63 @@ class Hashmap {
       currentNode.next = newNode;
     }
   }
+
+  get(key) {
+    let bucket = this.hash(key);
+    if (this.array[bucket] == null) {
+      return 'Key not found in array';
+    } else {
+      let currentNode = this.array[bucket];
+      while (currentNode !== null) {
+        if (currentNode.key == key) return currentNode.value;
+        else currentNode = currentNode.next;
+      }
+      return 'Key not found in array';
+    }
+  }
+
+  has(key) {
+    let bucket = this.hash(key);
+    if (this.array[bucket] == null) {
+      return false;
+    } else {
+      let currentNode = this.array[bucket];
+      while (currentNode !== null) {
+        if (currentNode.key == key) return true;
+        else currentNode = currentNode.next;
+      }
+      return false;
+    }
+  }
+
+  remove(key) {
+    let bucket = this.hash(key);
+    if (this.array[bucket] == null) {
+      return false;
+    } else if (this.array[bucket].key == key) {
+      let newNode = this.array[bucket];
+      if (newNode.next == null) {
+        this.array[bucket] = null;
+        return true;
+      } else {
+        this.array[bucket] = new Node(
+          this.array[bucket].next.key,
+          this.array[bucket].next.value,
+          this.array[bucket].next.next
+        );
+        return true;
+      }
+    } else {
+      let currentNode = this.array[bucket];
+      let prevNode = this.array[bucket];
+      while (currentNode !== null) {
+        if (currentNode.key == key) {
+          prevNode.next = currentNode.next;
+          return true;
+        } else prevNode = currentNode;
+        currentNode = currentNode.next;
+      }
+      return false;
+    }
+  }
 }
